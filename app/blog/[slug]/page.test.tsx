fa19ea90c54ccd0c8b493ab899c92b8432c0b455
kimduyen <kimduyennguyen95@gmail.com>
2024-05-07 22:30:42 +0700

    update

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -9,3 +9,21 @@ it("App Router: Works with dynamic route segments", () => {
   render(<Page params={{ slug: "Test" }} />);
   expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
 });
+
+type Params = {
+  params: {
+    slug: string
+  }
+}
+
+export async function generateMetadata({params}: Params){
+  return {title: `Post: ${params.slug}`}
+
+}
+
+export default function Page({params} : Params){
+  return <> <h1>Slug: {params.slug}</h1> 
+  <p>Hole Tex</p>
+  <p>Welcome to Ho Chi Minh</p>
+  </>
+}
